5581cc89c796358b1fceb7c021b5385227e2d21b
Certil Remy <certil@noukod.com>
2018-09-24 19:29:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,18 @@ def summon_captain_planet (planeteer_calls = ["earth", "wind", "fire", "water", 
   }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  return_values = []
+  array.each {|element|
+    if element.length>4
+      return true
+    end
+    return_values << false
+  }
+  if return_values.length == array.length
+    return false
+  end
 end
 
 def find_the_cheese# code an argument here
