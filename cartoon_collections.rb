084a66611b6a37608995941e9cd9dd2e686990a2
Certil Remy <certil@noukod.com>
2018-09-24 19:21:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(name = ["Doc", "Dopey", "Bashful", "Grumpy"])
 	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer_calls = ["earth", "wind", "fire", "water", "heart"])
+  planeteer_calls.each_with_index do |planet, index|
+		puts "#{index + 1} #{planet.upcase}!"
+	end
+  
 end
 
 def long_planeteer_calls# code an argument here
